51a0c19c78206f6fcc802fb3d8846afa284b03b0
Caio Finotti Bosco <dandinzin@gmail.com>
2025-09-26 15:25:22 -0300

    feat: create pagination

--- src/pages/Movies/index.vue
+++ src/pages/Movies/index.vue
@@ -3,6 +3,7 @@ import { defineComponent } from "vue";
 import CardMidia from "@/components/CardMidia.vue";
 import { getAllMovies } from "@/services/movies.ts";
 import { Movie } from "@/types/movies.ts";
+import { useMovieStore } from "@/stores/movies";
 
 export default defineComponent({
   components: {
@@ -11,15 +12,26 @@ export default defineComponent({
   data() {
     return {
       films: [],
+      currentPage: 1,
+      movieStore: useMovieStore(),
     };
   },
   async mounted() {
-    try {
-      this.films = await getAllMovies();
-    } catch (err) {
-      console.error("Erro ao carregar filmes:", err);
-      this.films = [];
-    }
+    await this.loadPage(this.currentPage);
+  },
+  methods: {
+    async loadPage(page: number) {
+      try {
+        this.films = await getAllMovies(page);
+      } catch (err) {
+        console.error("Erro ao carregar filmes:", err);
+        this.films = [];
+      }
+    },
+    changeCurrentPage(page: number) {
+      this.currentPage = page;
+      this.loadPage(page);
+    },
   },
 });
 </script>
@@ -35,6 +47,45 @@ export default defineComponent({
           :name="film.title"
           :urlPoster="'https://image.tmdb.org/t/p/w342' + film.poster_path"
         />
+      </div>
+      <div
+        class="d-flex justify-content-center align-items-center gap-2 mt-3 pb-9"
+      >
+        <button
+          v-if="currentPage - 2 > 0"
+          class="btn btn-outline-primary"
+          @click="changeCurrentPage(currentPage - 2)"
+        >
+          {{ currentPage - 2 }}
+        </button>
+
+        <button
+          v-if="currentPage - 1 > 0"
+          class="btn btn-outline-primary"
+          @click="changeCurrentPage(currentPage - 1)"
+        >
+          {{ currentPage - 1 }}
+        </button>
+
+        <button class="btn btn-primary fw-bold" disabled>
+          {{ currentPage }}
+        </button>
+
+        <button
+          v-if="currentPage + 1 <= movieStore.total_pages"
+          class="btn btn-outline-primary"
+          @click="changeCurrentPage(currentPage + 1)"
+        >
+          {{ currentPage + 1 }}
+        </button>
+
+        <button
+          v-if="currentPage + 2 <= movieStore.total_pages"
+          class="btn btn-outline-primary"
+          @click="changeCurrentPage(currentPage + 2)"
+        >
+          {{ currentPage + 2 }}
+        </button>
       </div>
     </div>
   </div>
--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,17 +1,21 @@
 import type { AxiosResponse } from "axios";
 import api from "./api";
 import type { Movie, MoviesResponse } from "@/types/types";
+import { useMovieStore } from "@/stores/movies";
+
+const movieStore = useMovieStore();
 
 export async function getPopularMovies(page = 1): Promise<Movie[]> {
-  const res: AxiosResponse<MoviesResponse> = await api.get("/movie/popular?language=pt-BR", {
+  const res: AxiosResponse<MoviesResponse> = await api.get("/movie/popular", {
     params: { page },
   });
   return res.data.results;
 }
 
 export async function getAllMovies(page = 1): Promise<Movie[]> {
-  const res: AxiosResponse<MoviesResponse> = await api.get("/discover/movie?language=pt-BR", {
+  const res: AxiosResponse<MoviesResponse> = await api.get("/discover/movie", {
     params: { page },
   });
+  movieStore.changeTotalPages(res.data.total_pages)
   return res.data.results;
 }
