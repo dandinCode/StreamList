103f9895efe1f18deb2c0ebd9d025d74d9c9924d
Caio Finotti Bosco <dandinzin@gmail.com>
2025-09-26 17:23:14 -0300

    feat: create functions for get languages and genres

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,6 +1,12 @@
 import type { AxiosResponse } from "axios";
 import api from "./api";
-import type { Movie, MoviesResponse } from "@/types/types";
+import type {
+    Genre,
+  Language,
+  Movie,
+  MoviesFilters,
+  MoviesResponse,
+} from "@/types/types";
 import { useMovieStore } from "@/stores/movies";
 
 const movieStore = useMovieStore();
@@ -12,10 +18,28 @@ export async function getPopularMovies(page = 1): Promise<Movie[]> {
   return res.data.results;
 }
 
-export async function getAllMovies(page = 1): Promise<Movie[]> {
+export async function getAllMovies(
+  page = 1,
+  filters?: MoviesFilters
+): Promise<Movie[]> {
   const res: AxiosResponse<MoviesResponse> = await api.get("/discover/movie", {
-    params: { page },
+    params: { page, ...filters },
   });
-  movieStore.changeTotalPages(res.data.total_pages)
+  movieStore.changeTotalPages(res.data.total_pages);
   return res.data.results;
+}
+
+export async function getAllLanguages(): Promise<Language[]> {
+  const res: AxiosResponse<Language[]> = await api.get(
+    "/configuration/languages",
+    {}
+  );
+  return res.data;
+}
+
+export async function getAllGenres(): Promise<Genre[]> {
+  const res: AxiosResponse<Genre[]> = await api.get("/genre/movie/list", {
+    params: { language: "pt-BR" },
+  });
+  return res.data;
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,24 @@ export interface MoviesResponse {
   total_pages: number;
   total_results: number;
 }
+
+export interface MoviesFilters {
+  title?: string;
+  original_title?: string;
+  with_genres?: string;
+  release_date?: string;
+  with_origin_country?: string;
+  adult?: boolean;
+  with_original_language?: string;
+}
+
+export interface Language {
+  iso_639_1: string;
+  english_name: string;
+  name: string;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
